9ea65986827333128ba3b9ea3d739bea9aabdafc
Han Ni <hanniaung164@gmail.com>
2026-08-26 17:49:40 +0800

    Finished 'Add files' button

--- MergeForm.cs
+++ MergeForm.cs
@@ -6,13 +6,16 @@ public class MergeForm : Form
     private Button addButton = new Button();
     public MergeForm()
     {
+        //UI for Form
         Text = "PDF Merger";
         Width = 480;
         Height = 420;
 
+        //UI for the files list box
         pdfListBox.Location = new Point(20,20);
         pdfListBox.Size = new Size(430, 220);
 
+        //UI for "Add File(s)" button
         addButton.Text = "Add File(s)";
         addButton.Location = new Point(20,250);
         addButton.Size = new Size(140,32);
@@ -25,6 +28,17 @@ public class MergeForm : Form
 
     private void AddButton_Click(object? sender, EventArgs e)
     {
-        pdfListBox.Items.Add("test.pdf");
+       // pdfListBox.Items.Add("test.pdf");
+       using OpenFileDialog openFile = new OpenFileDialog();
+       openFile.Filter = "PDF Files|*.pdf";
+       openFile.Multiselect = true;
+
+       if (openFile.ShowDialog() == DialogResult.OK)
+        {
+            foreach(string path in openFile.FileNames)
+            {
+                pdfListBox.Items.Add(path);
+            }
+        }
     }
 }
